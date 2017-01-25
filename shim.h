4b4384d1d7ee8d9cbdccc6daa89404b258ab3488
Harlan Haskins <harlan@harlanhaskins.com>
2017-01-25 16:13:09 -0500

    Added all headers

--- shim.h
+++ shim.h
@@ -5,8 +5,24 @@
 #define __STDC_LIMIT_MACROS
 
 #include <llvm-c/Analysis.h>
+#include <llvm-c/BitReader.h>
 #include <llvm-c/BitWriter.h>
 #include <llvm-c/Core.h>
+#include <llvm-c/Disassembler.h>
+#include <llvm-c/ErrorHandling.h>
 #include <llvm-c/ExecutionEngine.h>
-#include <llvm-c/Transforms/Scalar.h>
-#include <llvm-c/Transforms/IPO.h>
+#include <llvm-c/Initialization.h>
+#include <llvm-c/IRReader.h>
+#include <llvm-c/Linker.h>
+#include <llvm-c/LinkTimeOptimizer.h>
+#include <llvm-c/lto.h>
+#include <llvm-c/Object.h>
+#include <llvm-c/OrcBindings.h>
+#include <llvm-c/Support.h>
+#include <llvm-c/Target.h>
+#include <llvm-c/TargetMachine.h>
+#include <llvm-c/IPO.h>
+#include <llvm-c/PassManagerBuilder.h>
+#include <llvm-c/Scalar.h>
+#include <llvm-c/Vectorize.h>
+#include <llvm-c/Types.h>
